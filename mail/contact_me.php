d83267b12631862e0bdcda70779fe00f20c616af
Ivan <pysoccerp@hotmail.com>
2017-01-20 01:49:39 -0500

    Removed mail form php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
-<?php
+/* <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
+/* if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
@@ -22,5 +22,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: noreply@ivanmgonzalez.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+return true;	*/		
+?> 
